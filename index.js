d6bfef078d637c720681e766010065ddee07c34d
sirteddanson <knowledgeMED@gmail.com>
2022-01-23 23:24:52 -0500

    UPDATE fn

--- index.js
+++ index.js
@@ -51,8 +51,11 @@ const viewEmployees = () => {
 
 // CREATE: Add a department
 const addDepartment = () => {
-  const sql = `INSERT INTO department (name)
-    VALUES (?)`;
+  const sql = 
+    `
+    INSERT INTO department (name)
+    VALUES (?)
+    `;
   const params = ['Customer Service']; //body.name
   db.query(sql, params, (err, rows) => {
     if (err) {
@@ -63,8 +66,11 @@ const addDepartment = () => {
 };
 // CREATE: Add a role
 const addRole = () => {
-  const sql = `INSERT INTO role (title, salary, department_id)
-    VALUES (?,?,?)`;
+  const sql = 
+    `
+    INSERT INTO role (title, salary, department_id)
+    VALUES (?,?,?)
+    `;
   const params = ['Process Auditor', '78000', '1']; //body.title, body.salary, body.department_id
   db.query(sql, params, (err, rows) => {
     if (err) {
@@ -76,9 +82,28 @@ const addRole = () => {
 
 // CREATE: Add a employee
 const addEmployee = () => {
-  const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
-    VALUES (?,?,?,?)`;
+  const sql = 
+    `
+    INSERT INTO employee (first_name, last_name, role_id, manager_id)
+    VALUES (?,?,?,?)
+    `;
   const params = ['Tom', 'Williams', '8', '2']; //body.first_name, body.last_name, body.role_id, body.manager_id
+  db.query(sql, params, (err, rows) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(rows);
+  });
+};
+
+// UPDATE: Update an employee role
+const updateRole = () => {
+  const sql = 
+    `
+    UPDATE employee SET role_id = ?
+    WHERE id = ?;
+    `;
+  const params = ['10', '4']; //req.body.role_id, req.params.id
   db.query(sql, params, (err, rows) => {
     if (err) {
       console.log(err);
